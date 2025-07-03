41fb56c995b86d014f8ccda9193d8458d0f171c4
Tawicha Sirisuwan <tawicha9643@outlook.com>
2025-07-04 00:25:36 +0700

    ลองเชื่อม FastAPI + MongoDB

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from database import db
 
 app = FastAPI()
 
+# ------------------- Models -------------------
+class User(BaseModel):
+    name: str
+    age: int
+
+# ------------------- Routes -------------------
+
 @app.get("/")
-
 def read_root():
-    return{"message" : "Hello World"}
+    return {"message": "Hello World"}
 
-@app.get('/hello')
-def read_hello():
-    return {"message": "Hello from FastAPI"}
+@app.post("/users")
+async def create_user(user: User):
+    result = await db.users.insert_one(user.dict())
+    return {"id": str(result.inserted_id), "message": "User created"}
 
-@app.get('/items/{id}')
-def read_item(id: int):
-    return {"item_id": id, "message": "This is an item"}
+@app.get("/users")
+async def get_users():
+    users = []
+    async for index, user in enumerate(db.users.find(), start=1):
+        users.append({
+            "id": index,               # เปลี่ยน _id เป็นเลขลำดับ
+            "name": user["name"],
+            "age": user["age"]
+        })
+    return users
